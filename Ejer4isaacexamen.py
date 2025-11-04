08b9638f01178109372bca34937c7e83c2e5e0bd
Isaac <irodriguezsolla@danielcastelao.org>
2025-11-04 14:02:56 +0100

    definición_bucle

--- Ejer4isaacexamen.py
+++ Ejer4isaacexamen.py
@@ -1,7 +1,7 @@
 list_temperaturas = []
 list_dias = ["Lunes", "Martes", "Miercoles", "Jueves", "Viernes", "Sabado", "Domingo"]
 suma = 0
-# Primero creamos una lista vacia y ota con los dias de la semana.
+'''El siguiente bucle lo que hace es pededir un numero 7 veces moviendonos en la lista de dias de la semana '''
 for i in list_dias:
     temp = int(input("Dime la temperatura de: "))
     list_temperaturas.append(temp)
@@ -9,6 +9,7 @@ for i in list_dias:
 
 media_temperaturas = suma /7
 print("La temperatura media es: " ,str(media_temperaturas))
+
 '''La siguiente función lo que hace es calcular cual es la temperatura que supera la media en la lista de temperaturas'''
 def Superior(superior, medias):
         Super = []
