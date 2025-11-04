6ce2eba3878aa32b3b18632b4b4f8c4b7b47edee
Isaac <irodriguezsolla@danielcastelao.org>
2025-11-04 13:29:56 +0100

    1

---
+++ Ejer4isaacexamen.py
@@ -0,0 +1,22 @@
+temperaturas = []
+dias = ["Lunes","Martes","Miercoles","Jueves","Viernes","Sabado", "Domingo"]
+suma = 0
+for i in dias:
+    temp = int(input("Dime la temperatura de: "))
+    temperaturas.append(temp)
+    suma = temp + suma
+
+media = suma /7
+print("La temperatura media es: " ,str(media))
+
+def superior(superior,medias,dia):
+        super = []
+        for t in superior:
+            if t > medias:
+                super.append(t)
+
+        for l
+
+    print(dia[l],super[])
+
+print("La temperatura que se pasa de la media es: ", superior(temperaturas,media,dias))
